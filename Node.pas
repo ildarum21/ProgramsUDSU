d8fa9a51c48066f536557e992480e05e48545255
Ildar Mullakhmetov <ildarum.work@yandex.ru>
2020-05-23 12:25:18 +0400

    List

--- Node.pas
+++ Node.pas
@@ -218,7 +218,7 @@ procedure deleteaNodeWithoutaSubtree(Top: NodePtr);
               if Key in ['y','Y','н','Н'] then
                 begin
                      ClrScr;
-                     Leaf^.Name:= Nil;
+                     Leaf:=Nil;
                     writeln('узел', Leaf^.Name,'удален');
                 end
              else
